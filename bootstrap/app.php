d0c35edee9f9995e05795c7c8092640e2959eae6
LOUIE NATHANIEL CHRISTOPHER <lnathaniel@student.ciputra.ac.id>
2026-05-14 22:45:36 +0700

    Add middleware to trust proxies in app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,3 +18,6 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+->withMiddleware(function ($middleware) {
+    $middleware->trustProxies(at: '*');
+})
