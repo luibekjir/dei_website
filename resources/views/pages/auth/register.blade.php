e61768f2af66f3d1877ce0055b4c5fe81d6f064b
dzaky nabil <mdzakynabil@student.ciputra.ac.id>
2026-05-20 01:13:00 +0700

    ketinggalan regis

--- resources/views/pages/auth/register.blade.php
+++ resources/views/pages/auth/register.blade.php
@@ -5,7 +5,14 @@
 
     <div class="space-x-1 rtl:space-x-reverse text-center text-sm text-zinc-600 dark:text-zinc-400 mt-6">
         <span>{{ __('Already have an account?') }}</span>
-        <flux:link :href="route('login')" wire:navigate>{{ __('Log in') }}</flux:link>
+        <flux:link 
+    :href="route('login')" 
+    wire:navigate
+    :accent="false"
+    class="!text-[#B25C18] hover:!text-[#8F4C11] font-bold !opacity-100"
+>
+    {{ __('Log in') }}
+</flux:link>
     </div>
     </div>
 </x-layouts::auth>
